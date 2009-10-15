fb61ad9e9d008f74673bd24cf76b4d4ef02cf57a
Greg Brown <gregplaysguitar@gmail.com>
2009-10-16 11:13:45 +1300

    tidied up a couple of things

--- demo/middleware.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import login, authenticate
-
-class LogGregInMiddleware(object):
-    def process_request(self, request):
-        u = authenticate(username='greg', password='password1')
-        if not request.user.is_authenticated():
-            login(request, u)
-        return None
--- demo/settings.py
+++ demo/settings.py
@@ -8,7 +8,7 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    ('Greg Brown', 'gregplaysguitar@gmail.com'),
+    ('Your name', 'youremail@mail.com'),
 )
 
 MANAGERS = ADMINS
