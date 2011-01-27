ba3a884e28c7470e06b99a35d4d7b711a018451d
Greg Brown <gregplaysguitar@gmail.com>
2011-01-27 16:35:27 +1300

    django deprecation warnings squashed, project choices now actually dynamic

--- demo/settings.py
+++ demo/settings.py
@@ -13,12 +13,22 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = '%s/demo.sqlite' % PROJECT_ROOT  # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '%s/demo.sqlite' % PROJECT_ROOT,
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
+
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -58,8 +68,8 @@ SECRET_KEY = '7l%-n5_i+k6ys*u#fmc*14z(*p(q&#2bdqicw9%b1@$t=^v+id'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.load_template_source',
 )
 
--- projectmanager/forms.py
+++ projectmanager/forms.py
@@ -2,9 +2,11 @@ from django import forms
 from models import Project, ProjectTime, Task
 
 
+def get_project_choices():
+    return (('Recent', [(p.pk, p.name) for p in Project.objects.filter(completed=False, hidden=False)]), ('Other', [(p.pk, p.name) for p in Project.objects.filter(completed=False, hidden=True)]))
 
 class ProjectTimeForm(forms.ModelForm):
-    project = forms.ChoiceField(choices=(('Recent', [(p.pk, p.name) for p in Project.objects.filter(completed=False, hidden=False)]), ('Other', [(p.pk, p.name) for p in Project.objects.filter(completed=False, hidden=True)])))
+    project = forms.ChoiceField(choices=get_project_choices())
     def clean_project(self):
         return Project.objects.get(pk=self.cleaned_data['project'])
     
@@ -14,6 +16,8 @@ class ProjectTimeForm(forms.ModelForm):
         
         # hack to allow field ordering - http://code.djangoproject.com/ticket/6369
         self.fields.keyOrder = self.Meta.fields
+        
+        self['project'].field.choices = get_project_choices()
 
     class Meta:
         model = ProjectTime
