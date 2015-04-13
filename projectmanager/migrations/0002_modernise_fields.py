d458ad23da49af964d63e34152c565ae7ce2e1e0
Greg Brown <gregplaysguitar@gmail.com>
2015-04-13 12:10:07 +1200

    Fix db integrity

--- projectmanager/migrations/0002_modernise_fields.py
+++ projectmanager/migrations/0002_modernise_fields.py
@@ -4,6 +4,9 @@ from __future__ import unicode_literals
 from django.db import models, migrations
 
 
+def fix_hours(apps, schema_editor):
+    Task = apps.get_model("projectmanager", "Task")
+    Task.objects.filter(estimated_hours__isnull=True).update(estimated_hours=0)
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -11,6 +14,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(fix_hours),
         migrations.AlterField(
             model_name='task',
             name='completed',
@@ -22,4 +26,3 @@ class Migration(migrations.Migration):
             field=models.DecimalField(default=0, null=True, max_digits=5, decimal_places=2),
         ),
     ]
-
