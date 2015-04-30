4699f9ac54a3f0e9dda49241f4268dc5274d95ec
Greg Brown <gregplaysguitar@gmail.com>
2015-05-01 11:49:23 +1200

    migration fix

--- hosting/migrations/0001_initial.py
+++ hosting/migrations/0001_initial.py
@@ -9,7 +9,7 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('projectmanager', '0008_auto_20150414_1003'),
+        ('projectmanager', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
